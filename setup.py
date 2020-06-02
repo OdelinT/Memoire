4cacc95ac4730a27d4c2904bf86dfa38cf75953a
OdelinT <odtamo@icloud.com>
2020-06-03 00:03:42 +0200

    add environment example

---
+++ setup.py
@@ -0,0 +1,22 @@
+from pileFaceEnvironment import pileFace
+from pileFaceEnvironment.pileFace import PileFace
+import tensorflow as tf
+from tf_agents.environments import utils
+
+tf.compat.v1.enable_v2_behavior()
+
+# Validate that the environment works
+environment = PileFace()
+utils.validate_py_environment(environment, episodes=5)
+
+environment = PileFace()
+time_step = environment.reset()
+print(time_step)
+cumulative_reward = time_step.reward
+
+for _ in range(10):
+    time_step = environment.step(True)
+    print(time_step)
+    cumulative_reward += time_step.reward
+
+print('Final Reward = ', cumulative_reward)
