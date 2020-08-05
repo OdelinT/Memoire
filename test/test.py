d0582a18343cb6aa59a2bd29b01932892d4479cd
OdelinT <odtamo@icloud.com>
2020-08-05 15:03:47 +0200

    improved output legibility

--- test/test.py
+++ test/test.py
@@ -1,3 +1,4 @@
+#region imports
 import unittest
 from tf_agents.environments import utils
 import tensorflow as tf
@@ -17,104 +18,104 @@ from tf_agents.replay_buffers import tf_uniform_replay_buffer
 from tf_agents.trajectories import trajectory
 from tf_agents.utils import common
 
+import coloredlogs, logging
 from tf_agents.agents.ddpg import critic_network
 from tf_agents.agents.sac import sac_agent
 
 import numpy as np
+#endregion
 
 # Test cases are commented and decommented according to what I was studying during commit.
 
 class test(unittest.TestCase):
     def setUp(self):
-        self.size = 10
-        self.duration = 10
-        # Overriding the default value of the parameters breaks utils.validate_py_environment
-        # because it will use the default parameters when sending actions to the environment, regardless the actual parameters
-        # self.env = PyEnv(self.size, self.duration)
+        coloredlogs.install(fmt='%(asctime)s %(levelname)s %(message)s')
         self.env = PyEnv()
         self.tf_env = tf_py_environment.TFPyEnvironment(self.env)
         self.train_env = self.tf_env
-    
+
+    """
     def testMyValidate(self):
         self.env._reset()
         #region Test environment parameters generation
+        logging.info("Test environment parameters generation")
         for value in self.env.placesSizes:
             if value < 0:
-                raise Exception()
+                logging.error("At least one place size < 0")
         for value in self.env.productsCosts:
             if value < 0:
-                raise Exception()
+                logging.error("At least one product cost < 0")
         for value in self.env.productsUsualMarginRates:
             if value < 0:
-                raise Exception()
+                logging.error("At least one margin rate < 0")
             if value > 1:
-                raise Exception()
+                logging.error("At least one margin rate > 1")
         for value in self.env.productsUsualBuyingRates:
-            if value < 0:
-                raise Exception()
+            if value <= 0:
+                logging.error("At least one buying rate <= 0")
         for i in range(len(self.env.productsUsualPrices)):
             if self.env.productsUsualPrices[i] < self.env.productsCosts[i]:
-                raise Exception()
+                logging.error("At least one usual price smaller than product cost")
         #endregion
 
         #region Test environment credibility with different prices
-        print("Test environment credibility with different prices")
+        logging.info("Test environment credibility with different prices")
         observation = self.env._step(self.env.productsCosts)
-        print("Product costs: ", observation)
+        logging.info(f"Product costs: {observation}")
         costReward = observation.reward
         if observation.reward != 0:
-            raise("Error: If we sell at product cost, there should be no margin")
+            logging.error("Error: If we sell at product cost, there should be no margin")
 
         observation = self.env._step(self.env.productsCosts * 2)
-        print("Product costs *2: ", observation)
+        logging.info(f"Product costs *2: {observation}")
         if observation.reward <= 0 and np.sum(observation.observation) > 0:
-            raise("Error: If we sell at more than product cost, there should be a margin")
+            logging.error("Error: If we sell at more than product cost, there should be a margin")
 
         observation = self.env._step(self.env.productsCosts * 10)
-        print("Product costs *10: ", observation)
+        logging.info(f"Product costs *10: {observation}")
         if observation.reward <= 0 and np.sum(observation.observation) > 0:
-            raise("Error: If we sell at more than product cost, there should be a margin")
+            logging.error("Error: If we sell at more than product cost, there should be a margin")
 
         observation = self.env._step(self.env.productsUsualPrices)
-        print("Usual buying price: ", observation)
+        logging.info(f"Usual buying price: {observation}")
         usualReward = observation.reward
 
         observation = self.env._step(self.env.productsUsualPrices * 2)
-        print("Usual buying price *2: ", observation)
-        print("Compared to usual: ", observation.reward / usualReward)
+        logging.info(f"Usual buying price *2: {observation}")
+        logging.info(f"Compared to usual: {observation.reward / usualReward}")
 
         observation = self.env._step(self.env.productsUsualPrices * 4)
-        print("Usual buying price *4: ", observation)
-        print("Compared to usual: ", observation.reward / usualReward)
+        logging.info(f"Usual buying price *4: {observation}")
+        logging.info(f"Compared to usual: {observation.reward / usualReward}")
 
         observation = self.env._step(self.env.productsUsualPrices * 10)
-        print("Usual buying price *10: ", observation)
-        print("Compared to usual: ", observation.reward / usualReward)
+        logging.info(f"Usual buying price *10: {observation}")
+        logging.info(f"Compared to usual: {observation.reward / usualReward}")
 
         observation = self.env._step(np.zeros(100))
-        print("0: ", observation)
+        logging.info(f"0: {observation}")
         if observation.reward >= 0.:
-            raise("Error: If prices are 0, we should sell and have a deficit")
+            logging.error("Error: If prices are 0, we should sell and have a deficit")
 
         observation = self.env._step(np.zeros(100) + 1)
-        print("1: ", observation)
+        logging.info(f"1: {observation}")
         if observation.reward >= 0:
-            raise("Error: If prices are 1 and average cost 10, we should sell and have a deficit")
+            logging.error("Error: If prices are 1 and average cost 10, we should sell and have a deficit")
 
         observation = self.env._step(np.zeros(100) + 1000)
-        print("1000: ", observation)
+        logging.info(f"1000: {observation}")
         if observation.reward > usualReward:
-            raise("Error: An unusual price such as 1000 shouldn't cause a best result than the usual price. Error ratio: ", observation.reward / usualReward)
+            logging.error("Error: An unusual price such as 1000 shouldn't cause a best result than the usual price. Error ratio: ", observation.reward / usualReward)
         #endregion
     
     def testValidate(self):
         utils.validate_py_environment(self.env, episodes=5)
+    """
     
-    
-    # Works!
+    # Works at least until initialization
     # https://github.com/tensorflow/agents/blob/master/docs/tutorials/6_reinforce_tutorial.ipynb
     def testReinforceActorDistributionNet(self):
-        print("Starting testReinforceActorDistributionNet")
+        logging.info("Starting testReinforceActorDistributionNet")
         #region Hyperparameters from the example of the documentation
         num_iterations = 250 # @param {type:"integer"}
         collect_episodes_per_iteration = 2 # @param {type:"integer"}
@@ -128,6 +129,7 @@ class test(unittest.TestCase):
         eval_interval = 50 # @param {type:"integer"}
         #endregion
 
+        #region Agent initialization
         actor_net = actor_distribution_network.ActorDistributionNetwork(
             self.train_env.observation_spec(),
             self.train_env.action_spec(),
@@ -144,13 +146,15 @@ class test(unittest.TestCase):
             normalize_returns=True,
             train_step_counter=train_step_counter)
         tf_agent.initialize()
+        #endregion
 
         collect_policy = tf_agent.collect_policy
 
-        print(self.compute_avg_return(self.train_env, collect_policy))
+        logging.info(self.compute_avg_return(self.train_env, collect_policy))
     
-    
+    # Works at least until initialization
     def testTd3(self):
+        logging.info("Starting testTd3")
         #region Hyperparameters from the example of the documentation
         fc_layer_params = (100,)
 
@@ -160,6 +164,7 @@ class test(unittest.TestCase):
         eval_interval = 50 # @param {type:"integer"}
         #endregion
         
+        #region Agent initialization
         actor_net = actor_distribution_network.ActorDistributionNetwork(
             self.tf_env.observation_spec(),
             self.tf_env.action_spec(),
@@ -171,58 +176,26 @@ class test(unittest.TestCase):
         
         actor_optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=learning_rate)
         critic_optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=learning_rate)
-
+        
         actor = tf_agents.agents.Td3Agent(
             time_step_spec = self.tf_env.time_step_spec(), 
-            action_spec = self.tf_env.action_spec,
+            action_spec = self.tf_env.action_spec(),
             actor_network = actor_net,
             critic_network = critic_net,
             actor_optimizer = actor_optimizer,
-            critic_optimizer = critic_optimizer, 
-            exploration_noise_std=0.1, critic_network_2=None,
-            target_actor_network=None, target_critic_network=None,
-            target_critic_network_2=None, target_update_tau=1.0, target_update_period=1,
-            actor_update_period=1, dqda_clipping=None, td_errors_loss_fn=None, gamma=1.0,
-            reward_scale_factor=1.0, target_policy_noise=0.2, target_policy_noise_clip=0.5,
-            gradient_clipping=None, debug_summaries=False, summarize_grads_and_vars=False,
-            train_step_counter=None, name=None)
+            critic_optimizer = critic_optimizer)
         actor.initialize()
+        #endregion
+
         collect_policy = actor.collect_policy
 
-        print(self.compute_avg_return(self.train_env, collect_policy))
-    
-    
-    def normal_projection_net(self, action_spec,init_means_output_factor=0.1):
-        return normal_projection_network.NormalProjectionNetwork(
-            action_spec,
-            mean_transform=None,
-            state_dependent_std=True,
-            init_means_output_factor=init_means_output_factor,
-            std_transform=sac_agent.std_clip_transform,
-            scale_distribution=True)
+        logging.info(self.compute_avg_return(self.train_env, collect_policy))
 
-    def compute_avg_return(self, environment, policy, num_episodes=5):
-        total_return = 0.0
-        print("Starting to compute")
-        for _ in range(num_episodes):
 
-            time_step = environment.reset()
-            episode_return = 0.0
-
-            while not time_step.is_last():
-                action_step = policy.action(time_step)
-                time_step = environment.step(action_step.action)
-                episode_return += time_step.reward
-            print(datetime.now(), " Episode return : ", episode_return)
-            total_return += episode_return
-        return total_return / num_episodes
-        # avg_return = total_return / num_episodes
-        # return avg_return.numpy()[0]
-
-    # Works!
+    # Works at least until initialization
     # https://github.com/tensorflow/agents/blob/master/docs/tutorials/7_SAC_minitaur_tutorial.ipynb
     def testSAC(self):
-        print("Starting testSAC")
+        logging.info("Starting testSAC")
         #region Hyperparameters from the example of the documentation
         # use "num_iterations = 1e6" for better results,
         # 1e5 is just so this doesn't take too long. 
@@ -252,6 +225,7 @@ class test(unittest.TestCase):
         eval_interval = 10000 # @param {type:"integer"}
         #endregion
         
+        #region Agent initialization
         observation_spec = self.train_env.observation_spec()
         action_spec = self.train_env.action_spec()
 
@@ -287,8 +261,36 @@ class test(unittest.TestCase):
             gradient_clipping=gradient_clipping,
             train_step_counter=global_step)
         tf_agent.initialize()
+        #endregion
 
         collect_policy = tf_agent.collect_policy
 
-        print(self.compute_avg_return(self.train_env, collect_policy))
+        logging.info(self.compute_avg_return(self.train_env, collect_policy))
+    
+    def normal_projection_net(self, action_spec,init_means_output_factor=0.1):
+        return normal_projection_network.NormalProjectionNetwork(
+            action_spec,
+            mean_transform=None,
+            state_dependent_std=True,
+            init_means_output_factor=init_means_output_factor,
+            std_transform=sac_agent.std_clip_transform,
+            scale_distribution=True)
+
+    def compute_avg_return(self, environment, policy, num_episodes=5):
+        total_return = 0.0
+        logging.info("Starting to compute")
+        for _ in range(num_episodes):
+
+            time_step = environment.reset()
+            episode_return = 0.0
+
+            while not time_step.is_last():
+                action_step = policy.action(time_step)
+                time_step = environment.step(action_step.action)
+                episode_return += time_step.reward
+            logging.info(f" Episode return : {episode_return}")
+            total_return += episode_return
+        return total_return / num_episodes
+        # avg_return = total_return / num_episodes
+        # return avg_return.numpy()[0]
     
